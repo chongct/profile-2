e80b294afbf7ede58c8125c16eefbd8cae75f82c
chongct <chongchingtong@gmail.com>
2020-08-31 23:39:16 +0800

    Add three.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { Component, createRef } from 'react';
+import * as THREE from 'three';
 
-const App = () => {
-  return (
-    <div>React app</div>
-  )
+import threeHelper from '../three';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.appRef = createRef();
+  }
+
+  componentDidMount() {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    this.appRef.current.appendChild(renderer.domElement);
+
+    threeHelper({ camera, renderer, scene });
+  }
+
+  render() {
+    return (
+      <div ref={this.appRef} />
+    )
+  }
 }
 
 export default App;
---
+++ src/three/index.js
@@ -0,0 +1,21 @@
+import * as THREE from '../../static/three';
+
+const threeHelper = ({ camera, renderer, scene }) => {
+  const geometry = new THREE.BoxGeometry();
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  camera.position.z = 5;
+
+  const animate = () => {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+  }
+
+  animate();
+}
+
+export default threeHelper;
